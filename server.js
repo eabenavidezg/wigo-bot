af0e6ac457556f51292fa363ddfaf5cba7e87ba7
eabenavidezg <108323895+eabenavidezg@users.noreply.github.com>
2026-08-25 23:36:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const app = express();
 
+const VERIFY_TOKEN = "wigo123";
+
 app.get("/", (req, res) => {
   res.send("WIGO Bot funcionando 🚖");
+});
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+
+  res.sendStatus(403);
 });
 
 const PORT = process.env.PORT || 3000;
